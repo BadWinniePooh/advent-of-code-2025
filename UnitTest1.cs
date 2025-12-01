e54667ac0295f1e3d9cf20087766e090c57f1409
BadWinniePooh <niklas@rueber.cloud>
2025-12-01 09:57:28 +0100

    🔒 - No rotation does not change starting position

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,5 +5,36 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var startingPosition = 50;
+        var sut = new Lock(startingPosition);
+
+        sut.Dial.Rotate("L0");
+        
+        Assert.Equal(50, 
+            sut.Dial.Position);
     }
+}
+
+public class Lock
+{
+    public Lock(int startingPosition)
+    {
+        Dial = new Dial(startingPosition);
+    }
+
+    public Dial Dial { get; set; }
+}
+
+public class Dial
+{
+    public Dial(int startingPosition)
+    {
+        Position = startingPosition;
+    }
+
+    public void Rotate(string input)
+    {
+    }
+
+    public int Position { get; set; }
 }
