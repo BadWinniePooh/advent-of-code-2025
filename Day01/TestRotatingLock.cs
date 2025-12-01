644b10f63fa5012fc6bbce355bac8ef5c7500f56
BadWinniePooh <niklas@rueber.cloud>
2025-12-01 11:20:14 +0100

    🔒✅ - introduce file based solving

--- Day01/TestRotatingLock.cs
+++ Day01/TestRotatingLock.cs
@@ -95,4 +95,34 @@ public class TestRotatingLock
         
         Assert.Equal(expectedPasswordInRiddle, sut.Dial.ExpectedPassword);
     }
+
+    [Fact]
+    public void LockSolver()
+    {
+        var expected = 3;
+        var startingPosition = 50;
+        var dialInput = File.ReadLines("./Day01/TestInput.txt");
+        var sut = new LockSolver(dialInput, startingPosition);
+        
+        var actual = sut.Solve();
+        
+        Assert.Equal(expected, actual);
+    }
+}
+
+public class LockSolver(IEnumerable<string> dialInput, int startingPosition)
+{
+    private IEnumerable<string> DialInput { get; set; } = dialInput;
+    private Lock Lock { get; set; } = new(startingPosition);
+
+    public int Solve()
+    {
+        var dial = Lock.Dial;
+        foreach (var input in DialInput)
+        {
+            dial.Rotate(input);
+        }
+
+        return dial.ExpectedPassword;
+    }
 }
