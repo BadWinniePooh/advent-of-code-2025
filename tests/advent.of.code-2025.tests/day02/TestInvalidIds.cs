346f3cb4481d4e9a9f3f5a9bbe5d9a630f596fb6
BadWinniePooh <niklas@rueber.cloud>
2025-12-02 09:21:04 +0100

    🐝 - GiftShopComputer can sum invalid ids for one id pair

--- tests/advent.of.code-2025.tests/day02/TestInvalidIds.cs
+++ tests/advent.of.code-2025.tests/day02/TestInvalidIds.cs
@@ -5,5 +5,44 @@ public class TestInvalidIds
     [Fact]
     public void TESTNAME()
     {
+        var sut = new GiftShopComputer();
+        var actual = sut.SumInvalidIds("12-34");
+        Assert.Equal(0, actual);
+    }
+
+    [Fact]
+    public void t2()
+    {
+        var sut = new GiftShopComputer();
+        var actual = sut.SumInvalidIds("11-34");
+        Assert.Equal(11, actual);
+    }
+    
+    [Fact]
+    public void t3()
+    {
+        var sut = new GiftShopComputer();
+        var actual = sut.SumInvalidIds("11-22");
+        Assert.Equal(33, actual);
+    }
+}
+
+public class GiftShopComputer
+{
+    public int SumInvalidIds(string unformattedIds)
+    {
+        var sum = 0;
+        var ids = unformattedIds.Split('-').ToList();
+
+        foreach (var id in ids)
+        {
+            var middle = id.Length / 2;
+            var firstPart = id[..middle];
+            var secondPart = id[middle..];
+            sum += firstPart == secondPart ? int.Parse(id) : 0;
+            
+        }
+        
+        return sum;
     }
 }
