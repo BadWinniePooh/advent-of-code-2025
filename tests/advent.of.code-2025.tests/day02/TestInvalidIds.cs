3ffe5cec32b9bdfd30ccaa6ffd99690e6a0bb830
BadWinniePooh <niklas@rueber.cloud>
2025-12-02 10:24:21 +0100

    🐝 - accept multiple id pairs 🎨 - introduce IdPair type

--- tests/advent.of.code-2025.tests/day02/TestInvalidIds.cs
+++ tests/advent.of.code-2025.tests/day02/TestInvalidIds.cs
@@ -25,24 +25,51 @@ public class TestInvalidIds
         var actual = sut.SumInvalidIds("11-22");
         Assert.Equal(33, actual);
     }
+
+    [Theory]
+    [InlineData("11-22,95-115,1212-4567", 1245)]
+    public void GiftShopComputerCanAcceptMoreThanOnePairOfIds(string input, int expectedSum)
+    {
+        var sut = new GiftShopComputer();
+        var actual = sut.SumInvalidIds(input);
+        Assert.Equal(expectedSum, actual);
+    }
 }
 
 public class GiftShopComputer
 {
     public int SumInvalidIds(string unformattedIds)
     {
-        var sum = 0;
-        var ids = unformattedIds.Split('-').ToList();
+        var idPairs = IdPair.ConvertFrom(unformattedIds.Split(','));
+        return idPairs.Sum(idPair => idPair.Verify());
+    }
+}
 
-        foreach (var id in ids)
-        {
-            var middle = id.Length / 2;
-            var firstPart = id[..middle];
-            var secondPart = id[middle..];
-            sum += firstPart == secondPart ? int.Parse(id) : 0;
-            
-        }
-        
+public record IdPair(string FirstId, string SecondId)
+{
+    public static IEnumerable<IdPair> ConvertFrom(IEnumerable<string> idPairs)
+    {
+        return idPairs.Select(idPair => Parse(idPair));
+    }
+
+    private static IdPair Parse(string idPair)
+    {
+        var pairSplitted = idPair.Split('-').ToList();
+        return new IdPair(pairSplitted.First(), pairSplitted.Last());
+    }
+
+    private int EvaluateId(string id)
+    {
+        var middle = id.Length / 2;
+        var firstPart = id[..middle];
+        var secondPart = id[middle..];
+        return firstPart == secondPart ? int.Parse(id) : 0;
+    }
+
+    public int Verify()
+    {
+        var sum = EvaluateId(FirstId);
+        sum += EvaluateId(SecondId);
         return sum;
     }
 }
