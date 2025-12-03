e33a3e6fb972c25e2d00832a1181ae21b5068350
BadWinniePooh <niklas@rueber.cloud>
2025-12-03 08:17:20 +0100

    🕯️ - Riddle Description Day 3 Part 1 - 🪫

---
+++ tests/advent.of.code-2025.tests/day03/TestPowerunit.cs
@@ -0,0 +1,15 @@
+﻿namespace advent.of.code_2025.tests.day03;
+
+public class TestPowerunit
+{
+    [Fact]
+    public void TESTNAME()
+    {
+        /*
+         * A powerunit contains a bank of batteries (string of batteries)
+         * A powerunit accepts a list of banks
+         * A powerunit can detect the highest combination in any bank
+         * The order of batteries may not be changed (8192 -> 92 would be the highest)
+         */
+    }
+}
