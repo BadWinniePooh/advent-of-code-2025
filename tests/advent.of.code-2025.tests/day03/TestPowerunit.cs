64141ddac21d1ac64e09c14df27d3e7e48656ebf
BadWinniePooh <niklas@rueber.cloud>
2025-12-03 08:26:44 +0100

    🪫 - The total joltage of a single bank of 2 batteries is the value of the two batteries

--- tests/advent.of.code-2025.tests/day03/TestPowerunit.cs
+++ tests/advent.of.code-2025.tests/day03/TestPowerunit.cs
@@ -3,13 +3,37 @@
 public class TestPowerunit
 {
     [Fact]
-    public void TESTNAME()
+    public void WhenTwoBatteriesArePresent_ThenTwoBatteriesAreReturned()
     {
+        var listOfBanks = new List<Bank>();
+        listOfBanks.Add(new Bank("98"));
+        var powerunit = new PowerUnit(listOfBanks);
+
+        var actual = powerunit.TotalJoltage();
+        
+        Assert.Equal(98, actual);
+
         /*
          * A powerunit contains a bank of batteries (string of batteries)
          * A powerunit accepts a list of banks
          * A powerunit can detect the highest combination in any bank
          * The order of batteries may not be changed (8192 -> 92 would be the highest)
          */
+    }
+}
+
+public class PowerUnit(List<Bank> listOfBanks)
+{
+    public int TotalJoltage()
+    {
+        return listOfBanks.First().Joltage();
+    }
+}
+
+public record Bank (string batteries)
+{
+    public int Joltage()
+    {
+        return int.Parse(batteries);
     }
 }
