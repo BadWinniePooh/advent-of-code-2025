4815183de4dc81df9f6276378daace7f1d5ba23e
BadWinniePooh <niklas@rueber.cloud>
2025-12-04 10:49:22 +0100

    🧻 - Day 4 Part 1 - Start of counting accessible paper rolls in storage unit 🧻 - Single paper roll in otherwise empty row is accessible

---
+++ tests/advent.of.code-2025.tests/day04/TestForklift.cs
@@ -0,0 +1,40 @@
+﻿namespace advent.of.code_2025.tests.day04;
+
+public class TestForklift
+{
+    [Fact]
+    public void PaperRollInEmptyColumnIsAccessible()
+    {
+        var expected = true;
+        var storage = new StorageUnit();
+        var location = new Coordinate(0, 1);
+        
+        var actual = storage.PaperRollIsAccessible(".@.", location);
+        
+        Assert.Equal(expected, actual);
+    }
+}
+
+public record Coordinate(int row, int column);
+
+
+public class StorageUnit
+{
+    private const char PaperRoll = '@';
+    private const char EmptySpace = '.';
+
+    public bool PaperRollIsAccessible(string s, Coordinate location)
+    {
+        var adjacentPaperRolls = 0;
+
+        for (var locationIndex = location.column - 1; locationIndex <= location.column + 1; locationIndex++)
+        {
+            if (s[locationIndex] == PaperRoll)
+            {
+                adjacentPaperRolls++;
+            }
+        }
+
+        return adjacentPaperRolls < 4;
+    }
+}
