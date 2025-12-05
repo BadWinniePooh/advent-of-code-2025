185686dd208ccd9811a50be38525311442050dea
BadWinniePooh <niklas@rueber.cloud>
2025-12-05 10:02:22 +0100

    🍕 - we can transfer the elves DB to our internal Data Structure

--- tests/advent.of.code-2025.tests/day05/TestIngredientManagement.cs
+++ tests/advent.of.code-2025.tests/day05/TestIngredientManagement.cs
@@ -3,8 +3,55 @@
 public class TestIngredientManagement
 {
     [Fact]
-    public void TESTNAME()
+    public void InitDatabaseStructuresInputData()
     {
-        Assert.True(true);
+        var dirtyDb = File.ReadAllLines("./Day05/TestInput.txt");
+        var sut = new Database();
+        sut.InitDatabase(dirtyDb);
+        
+        Assert.Equal(4, sut.FreshIngredientRanges.Count);
+        Assert.Equal(6, sut.IngredientsInStorage.Count);
     }
+}
+
+public class Database
+{
+    public void InitDatabase(string[] dirtyDb)
+    {
+        var switchDatabase = false;
+        foreach (var entry in dirtyDb)
+        {
+            if (entry == "")
+            {
+                switchDatabase = true;
+                continue;
+            }
+
+            if (switchDatabase)
+            {
+                IngredientsInStorage.Add(new(entry));
+            }
+            else
+            {
+                FreshIngredientRanges.Add(new(entry));
+            }
+            
+        }
+    }
+
+    public List<Ingredient> FreshIngredientRanges { get; } = [];
+
+    public List<IngredientRange> IngredientsInStorage { get; } = [];
+}
+
+public record IngredientRange(string range)
+{
+    private int Separator => range.IndexOf('-');
+    public int StartId => int.Parse(range[..Separator]);
+    public int EndId => int.Parse(range[Separator..]);
+}
+
+public record Ingredient(string ingredient)
+{
+    public int Id => int.Parse(ingredient);
 }
