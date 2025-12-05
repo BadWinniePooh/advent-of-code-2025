67a0e6461e94b6c8d094a0734911ce97fc31e845
BadWinniePooh <niklas@rueber.cloud>
2025-12-05 13:40:10 +0100

    🍕 - rework count logic for performance optimization

--- src/advent.of.code-2025/day05/IngredientExtensions.cs
+++ src/advent.of.code-2025/day05/IngredientExtensions.cs
@@ -12,11 +12,46 @@ public static class IngredientExtensions
         return new IngredientRange(input);
     }
 
-    private static List<Ingredient> _ingredients;
-    
-    public static void Add(this List<Ingredient> source, IngredientRange items)
+    public static long CountDistinct(this List<IngredientRange> ranges)
     {
-        _ingredients = source;
-        _ingredients.AddRange(items.ToList());
+        var mergedRanges = MergeRanges(ranges);
+        return mergedRanges.Sum(range => (range.EndId - range.StartId + 1));
+    }
+
+    private static List<IngredientRange> MergeRanges(List<IngredientRange> ranges)
+    {
+        // Sort ranges by start ID
+        var sortedRanges = ranges
+            .OrderBy(r => r.StartId)
+            .ToList();
+
+        var mergedRanges = new List<IngredientRange>();
+        
+        // Select first IngredientRange
+        var currentStart = sortedRanges[0].StartId;
+        var currentEnd = sortedRanges[0].EndId;
+        
+        // Iterate over Ranges (skip first)
+        for (var index = 1; index < sortedRanges.Count; index++)
+        {
+            var range = sortedRanges[index];
+
+            //if overlapping or adjacent, merge
+            if (range.StartId <= currentEnd + 1)
+            {
+                currentEnd = Math.Max(currentEnd, range.EndId);
+            }
+            //else add range to list and start new range
+            else
+            {
+                mergedRanges.Add(new IngredientRange($"{currentStart}-{currentEnd}"));
+                currentStart = range.StartId;
+                currentEnd = range.EndId;
+            }
+        }
+
+        // Add last range to list
+        mergedRanges.Add(new IngredientRange($"{currentStart}-{currentEnd}"));
+        return mergedRanges;
     }
 }
